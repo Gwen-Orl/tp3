f372f6697c3d17a8bf5b8f3901747c2ee35ff29e
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2025-03-06 08:13:16 +0100

    private_contents

--- Box.java
+++ Box.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
     class Box{
-        ArrayList<Thing> contents = new ArrayList<Thing>();
+        private ArrayList<Thing> contents = new ArrayList<Thing>();
         public void add(Thing truc){
         this.contents.add(truc);}
     }
