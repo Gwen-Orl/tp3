88dd499dd8914511f33dea7920c59f616849b7c2
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2025-03-06 09:19:13 +0100

    ajout_equals_pour_contains

--- Box.java
+++ Box.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
     class Box{
         private ArrayList<Thing> contents = new ArrayList<Thing>();
+        
         public void add(Thing truc){
         this.contents.add(truc);}
+        
+        public boolean verifier(Thing truc){
+            if (this.contents.contains(truc)){
+                return true;
+            }
+            return false;
+        }
+
+       
     }
+
+    
 
 
 
--- Thing.java
+++ Thing.java
@@ -3,4 +3,20 @@ public class Thing {
         public Thing(String name){
         this.name = name;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+
+    public boolean equals(Object objet){
+    if (objet== null) {return false;}
+    if (objet==this) {return true;}
+    if (objet instanceof Thing) {return false;
+    Thing tmp = (Thing)objet;
+    return this.name.equals(tmp.getName());
+    }
+    return false;
+ }
 }
